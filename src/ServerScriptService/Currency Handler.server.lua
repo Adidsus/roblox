ab2499a1f2922c62405bde754ac05bf14f2d3898
Adidsus <a.did.uspl@gmail.com>
2023-11-26 23:35:54 +0100

    ad

--- src/ServerScriptService/Currency Handler.server.lua
+++
@@ -1,6 +0,0 @@
-local player = game.Players.LocalPlayer
-local money = player:WaitForChild("leaderstats"):WaitForChild("Cash")
-
-while wait(2) do
-	money.Value = money.Value + 5
-end
--- src/StarterGui/UI/LocalScript.client.lua
+++
@@ -1,9 +0,0 @@
-local player = game.Players.LocalPlayer
-
-local money = player:WaitForChild("leaderstats"):WaitForChild("Cash")
-
-script.Parent.Currency.Text = money.Value
-
-money.Changed:Connect(function(newValue)
-    script.Parent.Currency.Text = newValue
-end)
